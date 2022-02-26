37ba3ec4693c1ef176772aea8612e33f60ba3e59
neil-kuldip <neilkuldip2425@gmail.com>
2022-02-26 10:42:52 -0500

    Initial setup

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Assignment-2-JavaScript-Methods
+
+## Members:
+
+Neil Kuldip -> [neil-kuldip](https://github.com/neil-kuldip)
+
+Ambonique Thomas -> [AmboThom](https://github.com/AmboThom)
+
+### Description
+Our file, methods.js contains user-defined functions that mimic the behavior of established 
+JavaScript and Array methods in order to recreate them.
+
+The assignment was done using JavaScript, where the function defintions were split and completed by
+each of the group members. 
+
+### Link
+TBD
---
+++ methods.js
@@ -0,0 +1,68 @@
+// Authors: Neil Kuldip, Ambonique Thomas
+// Description: File containing user-defined functions that mimic the behavior of established 
+//              JavaScript and Array methods in order to recreate them
+// Resources Referenced: https://developer.mozilla.org/en-US/
+
+// FOR EACH // --> Provided by Professor Lai
+Array.prototype.myEach = function() {
+    for (let i = 0; i < this.length; i++) {   // "this" keyword refers to the array being called.
+        if (this[i] === undefined) continue;
+        // callbackFn can take up to 3 input parameters:
+        // element
+        // element, index
+        // element, index, array
+        callbackFn(this[i], i, this);   // callbackFn is "console.log(x,i,myArray)" 
+    }
+};
+
+// MAP //
+Array.prototype.myMap = function() {
+
+};
+
+// SOME //
+Array.prototype.mySome = function() {
+
+};
+
+// REDUCE //
+Array.prototype.myReduce = function() {
+
+};
+
+// INCLUDES //
+Array.prototype.myIncludes = function() {
+
+};
+
+// INDEXOF //
+Array.prototype.myIndexOf = function() {
+
+};
+
+// PUSH // --> Provided by Professor Lai
+Array.prototype.myPush = function() {
+    let args_index = 0;  // Index of new element
+    let length = this.length;   // Length of "this" array
+    // The last element of "this" array is at length - 1
+    for (let i = length; i < length + args.length; i++) {
+        this[i] = args[args_index];  // Add new element to end of "this" array
+        args_index++;
+    }
+    return this.length;  // Return new length of "this" array
+};
+
+// LASTINDEXOF //
+Array.prototype.myLastIndexOf = function() {
+
+};
+
+// KEYS //
+Object.myKeys = function() {
+
+};
+
+// VALUES //
+Object.myValues = function() {
+
+};
